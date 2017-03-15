f325d2b7734568156cc5f3af752aab2f16396eb5
luanatsainsburys <luan.au@sainsburys.co.uk>
2017-03-15 16:50:56 +0000

    Try ReactDataGrid

---
+++ src/components/DataGridPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DataGridBasic from './DataGridBasic';
+
+const DataGridPage = () => {
+    return (
+        <div>
+            <DataGridBasic/>
+        </div>
+        );
+    };
+
+export default DataGridPage;

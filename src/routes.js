2c863a60dbdafe763edcddd483e715778d2088cb
luanatsainsburys <luan.au@sainsburys.co.uk>
2017-04-11 16:27:43 +0100

    Run 'npm rebuild node-sass' as I am using node 7 locally

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import AboutPage from './components/AboutPage';
 import CustomerSearchPage from './components/CustomerSearchPage';
-import CustomerPage from './components/CustomerPage';
 import NotFoundPage from './components/NotFoundPage';
 import DataGridPage from './components/DataGridPage';
 
@@ -13,7 +12,6 @@ export default (
     <IndexRoute component={CustomerSearchPage}/>
     <Route path="data-grid-basic" component={DataGridPage}/>
     <Route path="customer-search" component={CustomerSearchPage}/>
-    <Route path="customer" component={CustomerPage}/>
     <Route path="about" component={AboutPage}/>
     <Route path="*" component={NotFoundPage}/>
   </Route>
